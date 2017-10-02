53c879e037bbf2e457fa4948eb7de093dc6cf08f
Ian A. Mason <iam@csl.sri.com>
2017-10-02 14:07:12 -0700

    Starting point for updating the yices formula to require the (as yet untested) libpoly formula.

---
+++ Formula/libpoly.rb
@@ -0,0 +1,35 @@
+class Libpoly < Formula
+  desc "C library for manipulating polynomials"
+  homepage "https://github.com/SRI-CSL/libpoly"
+  url "https://github.com/SRI-CSL/libpoly/archive/v0.1.4.tar.gz"
+  sha256 "f2c4fbf0690df479b406c84332995e154e2e194382fbdf802a8bdda1c4bca19b"
+
+  depends_on "cmake" => :build
+  depends_on "gmp"
+
+  def install
+    cd "build" do
+      system "cmake", "..", "-DLIBPOLY_BUILD_PYTHON_API=OFF", "-DLIBPOLY_BUILD_STATIC=OFF", "-DLIBPOLY_BUILD_STATIC_PIC=OFF", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}"
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+    #include <poly/rational.h>
+    #include <stdlib.h>
+
+    int main(int argc, char *argv[]) {
+        lp_rational_t *q;
+        q = (lp_rational_t*)malloc(sizeof(lp_rational_t));
+        lp_rational_construct_from_double(q, 5);
+        printf("%lf", lp_rational_to_double(q));
+        return 0;
+    }
+    EOS
+
+    system ENV.cc, "test.c", "-o", "test", "-I#{include}/poly", "-lpoly"
+    assert_match "5.000000", shell_output("./test")
+  end
+end
---
+++ Formula/yices.rb
@@ -0,0 +1,48 @@
+class Yices < Formula
+  desc "The Yices SMT Solver"
+  homepage "http://yices.csl.sri.com/"
+  url "https://github.com/SRI-CSL/yices2/archive/Yices-2.5.3.tar.gz"
+  sha256 "4c8972f7c257c80426f7d22473d339b5eaf8ce00ac7663f19734fa57f7fa7f28"
+
+  bottle do
+    cellar :any
+    sha256 "e480423bc7fbf94a3ba512a4f672c66057905f9aaee061caf80ae84259ea0555" => :sierra
+    sha256 "da027117a516e23047f3baaf6c9fe2c986434bec34c64ddeb72a11832535db5c" => :el_capitan
+    sha256 "47be959ca49dd851401cc9dcddf29cf7fe0f5f9e59f8639255e405a25e51e1ca" => :yosemite
+    sha256 "d6e40dd842e89e7b83eba751644096dfc27e6e16d2a14c8cc6e3d13c56a75fa8" => :x86_64_linux
+  end
+
+  depends_on "cmake" => :build
+  depends_on "autoconf" => :build
+  depends_on "gperf"
+  depends_on "gmp"
+
+  def install
+    system "autoconf"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"lra.smt2").write <<-EOS.undent
+      ;; QF_LRA = Quantifier-Free Linear Real Arithemtic
+      (set-logic QF_LRA)
+      ;; Declare variables x, y
+      (declare-fun x () Real)
+      (declare-fun y () Real)
+      ;; Find solution to (x + y > 0), ((x < 0) || (y < 0))
+      (assert (> (+ x y) 0))
+      (assert (or (< x 0) (< y 0)))
+      ;; Run a satisfiability check
+      (check-sat)
+      ;; Print the model
+      (get-model)
+    EOS
+
+    assert_match "sat\n(= x 2)\n(= y (- 1))\n", shell_output("#{bin}/yices-smt2 lra.smt2")
+  end
+end
