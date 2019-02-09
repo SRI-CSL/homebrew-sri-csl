77d0a7aa0f095c0bf2f0a719fb3aff8e46bea4c4
Ian A. Mason <iam@csl.sri.com>
2019-02-08 16:30:58 -0800

    Error: Calling fails_with :gcc is disabled! There is no replacement.

--- Formula/llvm_lite.rb
+++ Formula/llvm_lite.rb
@@ -20,9 +20,9 @@ class LlvmLite < Formula
 
   # According to the official llvm readme, GCC 4.7+ is required
   # fails_with :gcc_4_0
-  fails_with :gcc
-  ("4.3".."4.6").each do |n|
-    fails_with :gcc => n
+  # fails_with :gcc
+  #  ("4.3".."4.6").each do |n|
+  # fails_with :gcc => n
   end
 
   def install
