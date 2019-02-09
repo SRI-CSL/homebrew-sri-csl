779df4dc49fb3d82a1c6ab97692acfa00dfd929a
Ian A. Mason <iam@csl.sri.com>
2019-02-08 16:29:54 -0800

    Error: Calling fails_with :gcc_4_0 is disabled! There is no replacement.

--- Formula/llvm_lite.rb
+++ Formula/llvm_lite.rb
@@ -19,7 +19,7 @@ class LlvmLite < Formula
   depends_on "cmake" => :build
 
   # According to the official llvm readme, GCC 4.7+ is required
-  fails_with :gcc_4_0
+  # fails_with :gcc_4_0
   fails_with :gcc
   ("4.3".."4.6").each do |n|
     fails_with :gcc => n
