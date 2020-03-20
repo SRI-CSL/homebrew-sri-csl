dddbffb44cf6f179bb563daab9b0d0726894c298
Ian A.Mason <iam.csl.sri.com>
2020-03-20 10:29:14 -0700

    audit tweaks to cudd.rb

--- Formula/cudd.rb
+++ Formula/cudd.rb
@@ -1,19 +1,17 @@
 class Cudd < Formula
-  desc "CUDD: CU Decision Diagram package - unofficial git mirror of http://vlsi.colorado.edu/~fabio/"
+  desc "CUDD: CU Decision Diagram package"
   homepage "https://github.com/ivmai/cudd"
   url "https://github.com/ivmai/cudd/archive/cudd-3.0.0.tar.gz"
   sha256 "5fe145041c594689e6e7cf4cd623d5f2b7c36261708be8c9a72aed72cf67acce"
 
-
   def install
     system "./configure", "CC=clang",
                           "CXX=clang++",
-                          "--enable-silent-rules", 
-                          "--enable-shared", 
+                          "--enable-silent-rules",
+                          "--enable-shared",
                           "--enable-obj",
                           "--prefix=#{prefix}"
     system "make"
-    system "make", "check"
     system "make", "install"
   end
 
