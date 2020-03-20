a46c10a2906e881c72d2267b32b46e2155c0d2ff
Ian A.Mason <iam.csl.sri.com>
2020-03-20 11:07:34 -0700

    first guess at cadical. Missing 'make install' target, adds excitement.

---
+++ Formula/cadical.rb
@@ -0,0 +1,18 @@
+class Cadical < Formula
+  desc "The CDCL SAT Solver CaDiCaL"
+  homepage "https://github.com/arminbiere/cadical"
+  url "https://github.com/arminbiere/cadical/archive/rel-1.2.1.tar.gz"
+  sha256 "0e5075d3cf84403f7ed010a289cfc495ae056aee0d4892fe11e11a395858d4a2"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    # damn thing doesn't have an install target
+    cp "./build/libcadical.ad", lib.to_s
+    cp "./build/cadical.hpp", include.to_s
+  end
+
+  test do
+    system "true"
+  end
+end
