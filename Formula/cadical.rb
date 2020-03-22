8ca5044d8faa4833490af5cab215955119eababd
Ian A.Mason <iam.csl.sri.com>
2020-03-22 13:40:41 -0700

    cadical's configure doesn't accept a prefix. Bizarre.

--- Formula/cadical.rb
+++ Formula/cadical.rb
@@ -5,7 +5,7 @@ class Cadical < Formula
   sha256 "0e5075d3cf84403f7ed010a289cfc495ae056aee0d4892fe11e11a395858d4a2"
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure"
     system "make"
     # damn thing doesn't have an install target
     cp "./build/libcadical.ad", lib.to_s
