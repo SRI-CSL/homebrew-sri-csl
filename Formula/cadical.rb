bc107f4c729d209f36527ecaa31652221f5a150d
Ian A.Mason <iam.csl.sri.com>
2020-03-25 12:15:17 -0700

    Build cadical from our own tar ball.

--- Formula/cadical.rb
+++ Formula/cadical.rb
@@ -1,18 +1,13 @@
 class Cadical < Formula
   desc "The CDCL SAT Solver CaDiCaL"
   homepage "https://github.com/arminbiere/cadical"
-  url "https://github.com/arminbiere/cadical/archive/rel-1.2.1.tar.gz"
-  sha256 "0e5075d3cf84403f7ed010a289cfc495ae056aee0d4892fe11e11a395858d4a2"
+  url "https://github.com/SRI-CSL/cadical/archive/rel-1.2.2.tar.gz"
+  sha256 "824d324979387064378da497f47920b140e14191c61e2250c1b7433e1c379992"
 
   def install
     system "./configure"
     system "make"
-    # damn thing doesn't have an install target
-    system "/bin/mkdir -p #{lib}"
-    system "/bin/mkdir -p #{include}"
-    system "/bin/cp ./build/libcadical.a #{lib}/libcadical.a"
-    system "/bin/cp ./src/cadical.hpp #{include}/cadical.hpp"
-    system "/bin/cp ./src/ccadical.h #{include}/ccadical.h"
+    system "make", "install", "PREFIX=#{prefix}"
   end
 
   test do
